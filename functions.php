8949a395ad3135d0620ad1cb9a6b9e6125670dbf
Coralie Cellier <coralie.cellier@outlook.fr>
2025-02-11 15:08:21 +0100

    Ajout menu

--- functions.php
+++ functions.php
@@ -3,6 +3,6 @@
 /* Menu princiapl*/ 
 
 function register_my_menu() {
-    register_nav_menu( 'main-menu' => __( 'Menu principal', 'text-domain' ) );
+    register_nav_menu( 'main-menu' __( 'Menu principal', 'text-domain' ) );
 }
 add_action( 'after_setup_theme', 'register_my_menu' );
